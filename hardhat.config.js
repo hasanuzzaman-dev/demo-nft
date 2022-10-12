087b5ea3eeff87f67bf61d0893901b83c6eef4dc
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2022-10-12 12:30:26 +0600

    deployed done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require('@nomiclabs/hardhat-waffle');
+
+//const {API_URL, PRIVATE_KEY} = process.env
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey : 'VII5VU2A2MMQNW52VC6DATE94N2N6PMGJZ'
+  }
+  
 };
